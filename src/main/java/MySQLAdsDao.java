7bee61ad3700ca5974b12dcd90d295ec9e5ac0be
James Ward <jimmywardmichael@gmail.com>
2023-02-09 15:22:40 -0600

    done

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,36 @@
+import com.codeup.adlister.dao.Config;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
